6134be0f1319f52d062268110b3c5e03b8bbc792
niniel <natalia.maria.rojas@gmail.com>
2023-06-09 00:17:04 -0600

    Proyecto base
    contiene el formulario, y obtiene la ubicacion

--- lab_4/lib/main.dart
+++ lab_4/lib/main.dart
@@ -1,125 +1,202 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_map/flutter_map.dart' as flutter_map;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Grúa App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LinkGeneratorScreen(),
+      routes: {
+        '/info': (context) => InfoScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class LinkGeneratorScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _LinkGeneratorScreenState createState() => _LinkGeneratorScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LinkGeneratorScreenState extends State<LinkGeneratorScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController carTypeController = TextEditingController();
+  TextEditingController carBrandController = TextEditingController();
+  TextEditingController carModelController = TextEditingController();
+  TextEditingController carColorController = TextEditingController();
+  TextEditingController carPlateController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String location = '';
+
+  void getLocation() async {
+    PermissionStatus status = await Permission.location.request();
+    if (status.isGranted) {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      setState(() {
+        location =
+            'Latitud: ${position.latitude}\nLongitud: ${position.longitude}';
+      });
+
+      String link = 'Información de la grúa:\n\n' +
+          'Nombre: ${nameController.text}\n' +
+          'Teléfono: ${phoneController.text}\n' +
+          'Tipo de carro: ${carTypeController.text}\n' +
+          'Marca: ${carBrandController.text}\n' +
+          'Modelo: ${carModelController.text}\n' +
+          'Color: ${carColorController.text}\n' +
+          'Placa: ${carPlateController.text}\n' +
+          'Ubicación: $location';
+      Navigator.pushNamed(context, '/info', arguments: {'link': link});
+    } else if (status.isDenied) {
+      // El usuario ha denegado el permiso de ubicación
+      // Puedes mostrar un diálogo o mensaje para informar al usuario
+      print('Permiso de ubicación denegado');
+    } else if (status.isPermanentlyDenied) {
+      // El usuario ha denegado permanentemente el permiso de ubicación
+      // Puedes mostrar un diálogo o mensaje y guiar al usuario a la configuración de la aplicación
+      print('Permiso de ubicación denegado permanentemente');
+    }
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    phoneController.dispose();
+    carTypeController.dispose();
+    carBrandController.dispose();
+    carModelController.dispose();
+    carColorController.dispose();
+    carPlateController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Generador de Link'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Nombre'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextField(
+              controller: phoneController,
+              decoration: InputDecoration(labelText: 'Teléfono'),
+              keyboardType: TextInputType.phone,
+            ),
+            TextField(
+              controller: carTypeController,
+              decoration: InputDecoration(labelText: 'Tipo de carro'),
+            ),
+            TextField(
+              controller: carBrandController,
+              decoration: InputDecoration(labelText: 'Marca'),
+            ),
+            TextField(
+              controller: carModelController,
+              decoration: InputDecoration(labelText: 'Modelo'),
+            ),
+            TextField(
+              controller: carColorController,
+              decoration: InputDecoration(labelText: 'Color'),
+            ),
+            TextField(
+              controller: carPlateController,
+              decoration: InputDecoration(labelText: 'Placa'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                getLocation();
+                String link = 'Información de la grúa:\n\n' +
+                    'Nombre: ${nameController.text}\n' +
+                    'Teléfono: ${phoneController.text}\n' +
+                    'Tipo de carro: ${carTypeController.text}\n' +
+                    'Marca: ${carBrandController.text}\n' +
+                    'Modelo: ${carModelController.text}\n' +
+                    'Color: ${carColorController.text}\n' +
+                    'Placa: ${carPlateController.text}\n' +
+                    'Ubicación: $location';
+                Navigator.pushNamed(context, '/info',
+                    arguments: {'link': link});
+              },
+              child: Text('Generar Link'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class InfoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, dynamic>? args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+
+    final String link = args?['link'] ?? '';
+
+    LatLng location = LatLng(0, 0);
+
+    Set<Marker> markers = Set<Marker>.from([
+      Marker(
+        markerId: MarkerId('locationMarker'),
+        position: location,
+      ),
+    ]);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Información'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Datos obtenidos:',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              'Link: $link',
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ),
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: location,
+                zoom: 15.0,
+              ),
+              markers: markers,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
